a71dc0ee0eb6341434f1cc90a157202723185152
SY <101923627+ShinYingChua@users.noreply.github.com>
2023-06-06 12:04:09 +0800

    Updated ML models

--- src/mlp.py
+++ src/mlp.py
@@ -9,20 +9,20 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
 
-
-# def LinearRegressionModel(X_train, X_test, y_train, y_test):
-#     model_LR = LinearRegression()
-#     model_LR.fit(X_train, y_train)
-#     y_pred = model_LR.predict(X_test)
-#     print(mean_absolute_percentage_error(y_test, y_pred))
-
-def RandomForestModel(X_train, X_test, y_train, y_test):
+def LinearRegressionModel(X_train, X_test, y_train, y_test):
+    model_LR = LinearRegression()
+    model_LR.fit(X_train, y_train)
+    y_pred = model_LR.predict(X_test)
+    print("Linear Regression mean abs % error: ",
+          mean_absolute_percentage_error(y_test, y_pred))
+    
+def RandomForestRegressionModel(X_train, X_test, y_train, y_test):
     model_RFR = RandomForestRegressor(n_estimators=10)
     model_RFR.fit(X_train, y_train)
     y_pred = model_RFR.predict(X_test)
-    print(mean_absolute_percentage_error(y_test, y_pred))
-
-
+    print("Random Forest Regression mean abs % error: ",
+          mean_absolute_percentage_error(y_test, y_pred))
+    
 def main():
     dataset = pd.read_excel("HousePricePrediction.xlsx")
 
@@ -48,9 +48,8 @@ def main():
         X, y, test_size=0.2, random_state=0)
 
     # ML Models
-    # LinearRegressionModel(X_train, X_test, y_train, y_test)
-    RandomForestModel(X_train, X_test, y_train, y_test)
-
+    LinearRegressionModel(X_train, X_test, y_train, y_test)
+    RandomForestRegressionModel(X_train, X_test, y_train, y_test)
 
 if __name__ == '__main__':
     main()
